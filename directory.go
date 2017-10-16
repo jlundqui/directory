11b5ac24b370b57e595ab19936fdbddb96867b42
jlundqui <jlundqui@gmail.com>
2017-10-16 02:51:03 -0500

    Expanded tests

--- directory.go
+++ directory.go
@@ -7,10 +7,13 @@ import (
 	"html"
 	"log"
 	"net/http"
+	// "strings"
 
 	"github.com/gorilla/mux"
+	// "logging"
 )
 
+// Contact contains data for directory entries.
 type Contact struct {
 	ID        string `json:"id,omitempty"`
 	Firstname string `json:"firstname,omitempty"`
@@ -19,13 +22,30 @@ type Contact struct {
 	Phone     string `json:"phone,omitempty"`
 }
 
+// var directoryJSON = `{"id": "1",  "firstname": "Ann", "lastname": "Adams",
+// 	 "email": "aadams@fakemail.net", "phone": "123-456-7890"},
+// 	 {"id": "2", "firstname": "Brenda", "lastname": "Bowman",
+// 		 "email": "bbowman@fakemail.net", "phone": "123-890-4567"}`
+
 var directory []Contact
 
-func IndexEndpoint(w http.ResponseWriter, req *http.Request) {
+// Equal compares contacts for equality.
+func (c Contact) Equal(contact Contact) bool {
+	if c.Firstname == contact.Firstname &&
+		c.Lastname == contact.Lastname &&
+		c.Email == contact.Email &&
+		c.Phone == contact.Phone {
+		return true
+	}
+
+	return false
+}
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(req.URL.Path))
 }
 
-func GetContactEndpoint(w http.ResponseWriter, req *http.Request) {
+func getContactHandler(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	for _, contact := range directory {
 		if contact.ID == params["id"] {
@@ -33,27 +53,30 @@ func GetContactEndpoint(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 	}
+
 	json.NewEncoder(w).Encode(&Contact{})
 }
 
-func GetDirectoryEndpoint(w http.ResponseWriter, req *http.Request) {
+func getDirectoryHandler(w http.ResponseWriter, req *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(directory)
 }
 
-//func CreateContactEndpoint(w http.ResponseWriter, req *http.Request) {
-//	params := mux.Vars(req)
-//	var contact Contact
-//	_ = json.NewDecoder(req.Body).Decode(&contact)
-//	contact.ID = params["id"]
-//	contact.Firstname = params["firstname"]
-//	contact.Lastname = params["lastname"]
-//	contact.Email = params["email"]
-//	contact.Phone = params["phone"]
-//	directory = append(directory, contact)
-//	json.NewEncoder(w).Encode(directory)
-//}
+func createContactHandler(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	var contact Contact
+	_ = json.NewDecoder(req.Body).Decode(&contact)
+	contact.ID = params["id"]
+	contact.Firstname = params["firstname"]
+	contact.Lastname = params["lastname"]
+	contact.Email = params["email"]
+	contact.Phone = params["phone"]
+	directory = append(directory, contact)
+	json.NewEncoder(w).Encode(directory)
+}
 
-func DeleteContactEndpoint(w http.ResponseWriter, req *http.Request) {
+func deleteContactHandler(w http.ResponseWriter, req *http.Request) {
 	params := mux.Vars(req)
 	for index, contact := range directory {
 		if contact.ID == params["id"] {
@@ -65,13 +88,30 @@ func DeleteContactEndpoint(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	// reader := strings.NewReader(directoryJSON)
+	// logging.Info.Printf("Reader length %v\n", reader.Len())
+	// if err := json.NewDecoder(reader).Decode(&directory); err != nil {
+	// 	logging.Error.Printf("Unable to create directory. Error: %v\n", err)
+	// }
+
+	fmt.Println("Server starting")
+	log.Fatal(http.ListenAndServe(":8081", Handlers()))
+}
+
+// Handlers - creates router and adds handlers
+func Handlers() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
-	directory = append(directory, Contact{ID: "1", Firstname: "Ann", Lastname: "Adams", Email: "aadams@fakemail.net"})
-	directory = append(directory, Contact{ID: "2", Firstname: "Brenda", Lastname: "Bowman"})
-	router.HandleFunc("/", IndexEndpoint)
-	router.HandleFunc("/directory", GetDirectoryEndpoint).Methods("GET")
-	router.HandleFunc("/contact/{id}", GetContactEndpoint).Methods("GET")
-	//	router.HandleFunc("/contact/{id}/{firstname}/{lastname}/{email}/{phone}", CreateContactEndpoint).Methods("POST")
-	router.HandleFunc("/contact/{id}", DeleteContactEndpoint).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":12345", router))
+	// directory = append(directory, Contact{ID: 1, Firstname: "Ann", Lastname: "Adams", Email: "aadams@fakemail.net"})
+	// directory = append(directory, Contact{ID: 2, Firstname: "Brenda", Lastname: "Bowman"})
+	directory = append(directory, Contact{ID: "1", Firstname: "Ann", Lastname: "Adams",
+		Email: "aadams@fakemail.net", Phone: "123-456-7890"})
+	directory = append(directory, Contact{ID: "2", Firstname: "Brenda", Lastname: "Bowman",
+		Email: "bbowman@fakemail.net", Phone: "123-890-4567"})
+	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/directory", getDirectoryHandler).Methods("GET")
+	router.HandleFunc("/contact/{id}", getContactHandler).Methods("GET")
+	router.HandleFunc("/contact/{firstname}/{lastname}/{email}/{phone}", createContactHandler).Methods("POST")
+	router.HandleFunc("/contact/{id}", deleteContactHandler).Methods("DELETE")
+
+	return router
 }
--- directory_test.go
+++ directory_test.go
@@ -1,22 +1,108 @@
 // directory_test
 package main
 
-//import (
-//	"encoding/json"
-//	"fmt"
-//	"html"
-//	"log"
-//	"net/http"
-//	"net/url"
-//	"testing"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
-//	"github.com/gorilla/mux"
-//)
+var (
+	server                   *httptest.Server
+	reader                   io.Reader
+	contactURL, directoryURL string
+)
 
-//const (
-//    baseURL string = ""
-//)
+func init() {
+	server = httptest.NewServer(Handlers())
 
-//func connect() {
+	directoryURL = fmt.Sprintf("%s/directory", server.URL)
+	contactURL = fmt.Sprintf("%s/contact", server.URL)
+}
 
-//}
+func TestCreateContact(t *testing.T) {
+	contactJSON := `{"firstname": "Jane", "lastname": "Jones",
+	 "email": "jjones@doesnotexist.org", "phone": "987-645-3210"}`
+
+	// convert string to reader
+	reader = strings.NewReader(contactJSON)
+
+	// create request with reader
+	request, err := http.NewRequest("POST", contactURL, reader)
+	if err != nil {
+		// failed to create request
+		t.Error(err)
+		return
+	}
+
+	// send request
+	_, err = http.DefaultClient.Do(request)
+
+	if err != nil {
+		// failed during request send
+		t.Error(err)
+		return
+	}
+}
+
+func TestGetContact(t *testing.T) {
+	// create and send request
+	getContactURL := fmt.Sprintf("%s/1", contactURL)
+	request, err := http.NewRequest("GET", getContactURL, nil)
+	response, err := http.DefaultClient.Do(request)
+
+	fmt.Println(response.Body)
+
+	if err != nil {
+		// failed during request send
+		t.Error(err)
+		return
+	}
+
+	expContact := Contact{ID: "1", Firstname: "Ann", Lastname: "Adams",
+		Email: "aadams@fakemail.net", Phone: "123-456-7890"}
+
+	var contact Contact
+	err = json.NewDecoder(response.Body).Decode(&contact)
+	if err != nil {
+		t.Error(err) //Failed to decode the response
+	}
+
+	if contact.Equal(expContact) {
+		t.Errorf("Response returned incorrect contact information. Expected %v, received %v",
+			expContact, contact)
+	}
+}
+
+//
+// func TestIndexHandler(t *testing.T) {
+// 	req, err := http.NewRequest("GET", "/health-check", nil)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+//
+// 	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+// 	rr := httptest.NewRecorder()
+// 	handler := http.HandlerFunc(HealthCheckHandler)
+//
+// 	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+// 	// directly and pass in our Request and ResponseRecorder.
+// 	handler.ServeHTTP(rr, req)
+//
+// 	// Check the status code is what we expect.
+// 	if status := rr.Code; status != http.StatusOK {
+// 		t.Errorf("handler returned wrong status code: got %v want %v",
+// 			status, http.StatusOK)
+// 	}
+//
+// 	// Check the response body is what we expect.
+// 	expected := `{"alive": true}`
+// 	if rr.Body.String() != expected {
+// 		t.Errorf("handler returned unexpected body: got %v want %v",
+// 			rr.Body.String(), expected)
+// 	}
+// }
